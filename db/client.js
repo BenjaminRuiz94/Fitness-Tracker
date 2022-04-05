6521af42687558976d683ca9fd13823f3d361ec2
Benjamin Ruiz <Benjaminvruiz@gmail.com>
2022-04-05 11:15:09 -0500

    work to database client and tables

--- db/client.js
+++ db/client.js
@@ -1,9 +1,16 @@
 // build and export your unconnected client here
 const { Client } = require("pg");
-const client = new Client(
-  process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev"
-);
+// const client = new Client(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev"
+// );
 
-module.exports = {
-  client,
-};
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+module.exports = client;
